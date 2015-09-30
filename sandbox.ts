8178f0f78713ebd84a1426272df0755fa1e65efa
Tobias Bitterli <tobias.bitterli@greenmatch.ch>
2015-09-30 19:08:14 +0200

    Implemented pi estimator

---
+++ sandbox.ts
@@ -0,0 +1,25 @@
+
+var start = new Date().getTime();
+var pi = estimatePi(10000000);
+var end = new Date().getTime();
+
+console.log(pi, (end - start) / 1000);
+
+function estimatePi(shots) {
+    var counter = 0;
+    var x;
+    var y;
+
+    for(let i = 0; i < shots; i++) {
+        x = Math.random();
+        y = Math.random();
+
+        var abstand = Math.sqrt(x * x + y * y);
+
+        if(abstand < 1) {
+            counter += 1;
+        }
+    }
+
+    return counter / shots * 4;
+}
