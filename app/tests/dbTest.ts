20c55dd07b8c00e494b658ea41387d08a7e6ffad
Tobias Bitterli <tobias.bitterli@greenmatch.ch>
2015-10-08 00:11:03 +0200

    created routeTestsFile

--- app/tests/dbTest.ts
+++ app/tests/dbTest.ts
@@ -1,23 +1,11 @@
 /// <reference path="../../typings/tsd.d.ts" />
 import {connect} from "mongoose";
 import {should, expect} from "chai";
-import * as request from "supertest";
-
-import {app} from "../../server";
 
 connect("mongodb://localhost:27017/testing");
 
-describe("dbTest", function(){
-	it("get info", function(done) {
-		request(app)
-		.get("/app")
-		.expect(200, {
-		"version": "0.0.1",
-		"authors": "Jan Dietrich, Tobias Bitterli, Dario Knorr"
-		}, done);
-	});		
-	
+describe("dbTest", function(){	
 	it("simple test", function() {
-		expect(0).to.eql(0);
+		expect().to.eql(0);
 	})
 });
---
+++ app/tests/routeTests.ts
@@ -0,0 +1,18 @@
+/// <reference path="../../typings/tsd.d.ts" />
+import {connect} from "mongoose";
+import {should, expect} from "chai";
+import * as request from "supertest";
+
+import {app} from "../../server";
+
+
+describe("routeTests", function(){
+	it("get info", function(done) {
+		request(app)
+		.get("/app")
+		.expect(200, {
+		"version": "0.0.1",
+		"authors": "Jan Dietrich, Tobias Bitterli, Dario Knorr"
+		}, done);
+	});		
+});
