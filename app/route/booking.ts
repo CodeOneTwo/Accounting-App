3e6eff2d56b4192e58438f4ed8dec4dd273632b7
Jan Dietrich <jan.dietrich@sft-basel.ch>
2015-10-07 22:27:16 +0200

    bookings.ts

--- app/route/booking.ts
+++ app/route/booking.ts
@@ -1,11 +1,11 @@
 /// <reference path="../../typings/tsd.d.ts" />
 
-import {listAccounts, createAccount, getAccount, updateAccount, removeAccount} from "../controller/accounts";
+import {listBookings, createBooking, getBooking, updateBooking, removeBooking} from "../controller/bookings";
 
 var app = express.Router();
 
-app.get("/", listAccounts)
-.post("/", createAccount)
-.get("/:accountId", getAccount)
-.put("/:accountId", updateAccount)
-.delete("/:accountId", removeAccount);
+app.get("/", listBookings)
+.post("/", createBooking)
+.get("/:bookingId", getBooking)
+.put("/:bookingId", updateBooking)
+.delete("/:bookingId", removeBooking);
